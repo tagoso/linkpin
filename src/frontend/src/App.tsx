0c043a56b517b2f607d30c2d6d49dea381744093
tagoso <shirouo4279@gmail.com>
2024-11-17 17:48:48 +0200

    login screen max-h-full

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           <Entry />
         </div>
       ) : (
-        <div className="flex flex-col items-center justify-center flex-grow">
+        <div className="flex flex-col items-center justify-center flex-grow max-h-full">
           <Logo className="w-60 h-auto" />
           <LoginButton />
           <LoginInfo className="w-60 h-auto" />
