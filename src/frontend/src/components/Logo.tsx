0decb8d1abd8f2d005d84da222f1660827452633
tagoso <shirouo4279@gmail.com>
2024-11-18 16:07:14 +0200

    Logo balance

--- src/frontend/src/components/Logo.tsx
+++ src/frontend/src/components/Logo.tsx
@@ -1,6 +1,6 @@
 export default function Logo({ className }: { className?: string }) {
   return (
-    <div className={`flex flex-col items-center w-72 h-auto ${className}`}>
+    <div className={`flex flex-col items-center w-64 h-auto ${className}`}>
       <div className="flex items-center mt-2">
         <img alt="Long Linkpin Logo" className="mb-6" src="/logoLong.png" />
       </div>
